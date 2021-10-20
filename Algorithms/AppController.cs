a072dec0ac1aadbfeb5b0e774c6cffcea47befdd
Maciej Marzec <dzyzusek@gmail.com>
2021-10-20 10:37:56 +0200

    n/c
    n/c

---
+++ Algorithms/AppController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Algorithms
+{
+    public class AppController
+    {
+        public void IntroduceUser()
+        {
+            Console.WriteLine("Aplikacja jest zbiorem algorytmów.\nUżytkownik za pomocą instrukcji sterujących może wywoływać kolejne algorytmy.");
+        }
+        public void DoSomeAction()
+        {
+            
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppController appController = new AppController();
+
+            appController.IntroduceUser();
+        }
+    }
+}
